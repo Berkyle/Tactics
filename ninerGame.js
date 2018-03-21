acff9f6b6e7ef4d874b0bc2785f22c91a7f13554
Kyle <berkyle@ku.edu>
2018-03-20 21:58:51 -0500

    Added case for when nobody won a sub-table after 9 entries. Added case for when a player is sent to a full board. Still need to add handling for when user tries to use neutral board to win game.

--- ninerGame.js
+++ ninerGame.js
@@ -1,15 +1,17 @@
 //Board indices are 0-8...
 
 //TODO
-//add case for sent to a board that is already full
-//Add handling in checkTriple() for neutral Board
+//-X  add case for nobody won the board
+//-X  add case for sent to a board that is already full
+//->  Add handling in checkTriple() for neutral Board
 
 var tables = document.getElementsByClassName("subtable");
 var XO = document.getElementsByClassName("board");
 var txt = [];
+var nextPick = 9;
 var isX = true;
 var selClass = "";
-var gameTxt = [];
+var gameTxt = []; //undefined, 'X', 'O', or 'A'
 var possibleWins = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
 function addEvents() {
@@ -25,16 +27,15 @@ function addEvents() {
 				isX = !isX; //Boolean determines turn; (isX === true) => X's turn
 				txt[i] = XO[i].innerText;
 				grayOthers(i);
-				checkBoardWin(i);
+				checkBoardStatus(i);
 				checkGameWin();
       }
 		});
 	}
 }
 
-function checkBoardWin(i) {
-	var toSelect = Math.floor(i/9);
-	var check;
+function checkBoardStatus(tile) {	//i is the tile that the last choice was selected in
+	var toSelect = Math.floor(tile/9); //toSelect is the board that the last choice was selected in
 	if(gameTxt[toSelect] == undefined) { //make sure board hasn't already been won
 		possibleWins.forEach(function(poss) {
 			if(checkTriple(poss[0]+9*toSelect, poss[1]+9*toSelect, poss[2]+9*toSelect, txt)){
@@ -47,17 +48,21 @@ function checkBoardWin(i) {
 			}
 		});
 	}
+	if(checkBoardFull(toSelect)) {
+		gameTxt[toSelect] = 'A';
+		tables[toSelect].classList.add("Awinner");
+	}
+	if(checkBoardFull(tile%9)) removeGrayedAll();
 }
 
 function checkGameWin() {
 	possibleWins.forEach(function(poss) {
 		if(checkTriple(poss[0], poss[1], poss[2], gameTxt)) {
 			for(let i = 0; i < 81; i++) {
-				XO[i].classList.remove("grayed");
-				XO[i].classList.remove("OSelect","XSelect");
+				XO[i].classList.remove("grayed","OSelect","XSelect");
 				XO[i].classList.add(gameTxt[poss[0]]+"Select","selected");
 			}
-			var otherGuy = (gameTxt[poss[0]] == 'X') ? 'O' : 'X';
+			var otherGuy = (gameTxt[poss[0]] === 'X') ? 'O' : 'X';
 			for(let i = 0; i < 9; i++) {
 				tables[i].classList.remove(otherGuy+"winner");
 				tables[i].classList.add(gameTxt[poss[0]]+"winner");
@@ -67,8 +72,17 @@ function checkGameWin() {
 	});
 }
 
-function checkTriple(x1, x2, x3, array) {
-	if((array[x1] == array[x2] && array[x2] == array[x3]) && array[x1] != undefined) {
+function checkBoardFull(board) {
+	var numSelected = 0;
+	for(var i = 0; i < 9; i++) {
+		if (txt[i+9*board] != undefined) numSelected++;
+	}
+	if (numSelected === 9) return true;
+	return false;
+}
+
+function checkTriple(x1, x2, x3, arr) {
+	if((arr[x1] === arr[x2] && arr[x2] === arr[x3]) && arr[x1] !== undefined && arr[x1] !== 'A') {
 		return true;
 	}
 }
@@ -77,7 +91,7 @@ function grayOthers(dontGray) {
 	var toSelect = dontGray%9;
 	removeGrayedAll();
 	for(var i = 0; i < 81; i++) {
-		if(!XO[i].classList.contains("selected") && Math.floor(i/9) != toSelect) {
+		if(!XO[i].classList.contains("selected") && Math.floor(i/9) !== toSelect) {
 			XO[i].classList.add("grayed");
 		}
 	}
