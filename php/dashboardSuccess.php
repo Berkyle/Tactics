7ba1ca0ecde522c7fba61c3ce75563f47dff7e39
Kyle Berkley <berkyle@ku.edu>
2018-04-02 16:42:35 -0500

    Some minor refactoring, added feature list in dashboard.

--- php/dashboardSuccess.php
+++ php/dashboardSuccess.php
@@ -15,17 +15,21 @@ $password = $_SESSION["password"];
     <td><h5>Start 9x9 Game (Local)</h5></td>
   </tr>
   <tr>
+    <td><h5>Start 3x3 Game Against Bot (Local)</h5></td>
+    <td><h5>Start 9x9 Game Against Bot (Local)</h5></td>
+  </tr>
+  <tr>
     <td><h5>Start 3x3 Game (Online)</h5></td>
-    <td><h5>Start 9x9 Game (Online)</h5></td>
+    <td><h5>Start 9x9 Game (Online) (Coming Soon!)</h5></td>
   </tr>
   <tr>
     <td><h5>Continue 3x3 Game (Online)</h5></td>
-    <td><h5>Continue 9x9 Game (Online)</h5></td>
+    <td><h5>Continue 9x9 Game (Online)(Coming Soon!)</h5></td>
   </tr>
 </table>
 
-<a href=\"../html/profile.html\"><h3>-TODO- View my profile -TODO-</h3></a>
-<a href=\"logout.php\"><h3>Log out</h3></a>
+<a href="../html/profile.html"><h3>-TODO- View my profile -TODO-</h3></a>
+<a href="logout.php"><h3>Log out</h3></a>
 <div class="rankings">
   Eventually this will be full o' rankings
 </div>
