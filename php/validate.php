9c0451b5a54c98e236c40898cf73f52647b7683d
Kyle Berkley <berkyle@ku.edu>
2018-04-07 19:57:57 -0500

    User is now tracked via cookies rather than the global  variable.

--- php/validate.php
+++ php/validate.php
@@ -2,26 +2,26 @@
   require_once 'config.php';
   include_once 'header.php';
 
-  if($_COOKIE["user"] != ""){
-    $username = $_COOKIE["user"];
-    $password = $_COOKIE["pw"];
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
     $check = mysqli_query($link, "SELECT * FROM GameUsers WHERE username='$username'");
     $found = mysqli_num_rows($check);
 
     if($found > 0) { //User exists
       if($password == mysqli_fetch_object($check)->password) {
-        include 'rankings.php';
-        echo "<h1>Login successful! Welcome $username !</h1>";
-        include 'dashboardSuccess.php';
+        setcookie("user", $username, time()+(86400*10), "/");
+        setcookie("pw", $password, time()+(86400*10), "/");
+        header('Location: dashboard.php');
       }
       else{
-        echo "<h3>Error accessing credentials...</h3>
+        echo "<h3>That password is not correct...</h3>
               <h5><a href=\"../index.html\">Return to landing page...</a></h5>";
       }
     }
     else {
-      echo "<h3>Error accessing credentials....</h3>
+      echo "<h3>No user found with that username....</h3>
             <h5><a href=\"../index.html\">Return to landing page...</a></h5>";
     }
   }
