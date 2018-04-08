86e1a08d205f6ed68e8923a71c3b094e9903e08c
Kyle Berkley <berkyle@ku.edu>
2018-04-07 20:01:52 -0500

    Changed styling, restructured divs to make Leaderboard visible, buttons more appealing, etc.

--- php/dashboardSuccess.php
+++ php/dashboardSuccess.php
@@ -1,36 +1,42 @@
-<link rel="stylesheet" href="../css/index.css">
-
 <?php
 $username = $_SESSION["username"];
 $password = $_SESSION["password"];
 ?>
 
-<table>
-  <tr>
-    <th><h3>3x3 Board</h3></th>
-    <th><h3>9x9 Board</h3></th>
-  </tr>
-  <tr>
-    <td><h5>Start 3x3 Game (Local)</h5></td>
-    <td><h5>Start 9x9 Game (Local)</h5></td>
-  </tr>
-  <tr>
-    <td><h5>Start 3x3 Game Against Bot (Local)</h5></td>
-    <td><h5>Start 9x9 Game Against Bot (Local)</h5></td>
-  </tr>
-  <tr>
-    <td><h5>Start 3x3 Game (Online)</h5></td>
-    <td><h5>Start 9x9 Game (Online) (Coming Soon!)</h5></td>
-  </tr>
-  <tr>
-    <td><h5>Continue 3x3 Game (Online)</h5></td>
-    <td><h5>Continue 9x9 Game (Online)(Coming Soon!)</h5></td>
-  </tr>
-</table>
-
-<a href="../html/profile.html"><h3>-TODO- View my profile -TODO-</h3></a>
-<a href="logout.php"><h3>Log out</h3></a>
-<div class="rankings">
-  Eventually this will be full o' rankings
+<div class="bootstrapContainer">
+  <div class="bootstrapDiv bs1">
+    <div class="gameContainer">
+      <a href="../demos/singleBoardDemo/simpleBoard.html"><h5>Start 3x3 Game (Local)</h5></a>
+    </div>
+    <div class="gameContainer">
+      <a href="../demos/nineBoardDemo/ninerBoard.html"><h5>Start 9x9 Game (Local)</h5></a>
+    </div>
+  </div>
+  <div class="bootstrapDiv bs2">
+    <div class="gameContainer">
+      <h5>Start 3x3 Game Against Bot (Local)</h5>
+    </div>
+    <div class="gameContainer">
+      <h5>Start 9x9 Game Against Bot (Local)</h5>
+    </div>
+  </div>
+  <div class="bootstrapDiv bs3">
+    <div class="gameContainer">
+      <h5>Start 3x3 Game (Online)</h5>
+    </div>
+    <div class="gameContainer">
+      <h5>Start 9x9 Game (Online) (Coming Soon!)</h5>
+    </div>
+  </div>
+  <div class="bootstrapDiv bs4">
+    <div class="gameContainer">
+      <h5>Continue 3x3 Game (Online)</h5>
+    </div>
+    <div class="gameContainer">
+      <h5>Continue 9x9 Game (Online)(Coming Soon!)</h5>
+    </div>
+  </div>
 </div>
-<h5><a href="../index.html">Return to landing page...</a></h5>
+<br>
+<a href="../php/myProfile.php"><button type="button"><h3>View my profile</h3></button></a>
+<a href="logout.php"><button type="button"><h3>Log out</h3></button></a>
