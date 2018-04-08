a8453f13238a3762514c6db35b934e2401e0a962
Kyle Berkley <berkyle@ku.edu>
2018-04-07 19:50:28 -0500

    HTML templates to be added at the top and bottom of several .php files.

---
+++ php/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Tactics</title>
+    <link rel="stylesheet" href="../css/index.css">
+  </head>
+  <body>
+    <div class="container">
