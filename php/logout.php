0021d30f7df90beb70e276c3dfd9081cf520789c
Kyle Berkley <berkyle@ku.edu>
2018-04-07 20:04:00 -0500

    Logout now removes cookies by changing value to empty string.

--- php/logout.php
+++ php/logout.php
@@ -1,18 +1,11 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Tactics</title>
-  </head>
-  <body>
-    <?php
-    // remove all session variables
-    session_unset();
+<?php
+  include_once 'header.php';
+  setcookie("user", "", time()+(86400*10), "/");
+  setcookie("pw", "", time()+(86400*10), "/");
 
-    // destroy the session
-    session_destroy();
-    ?>
-    <h1>You have logged out successfuly. Come back soon!</h1>
-    <h5><a href="../index.html">Return to landing page...</a></h5>
-  </body>
-</html>
+  ?>
+  <h1>You have logged out successfuly. Come back soon!</h1>
+  <h5><a href="../index.html">Return to landing page...</a></h5>
+  <?php
+  include_once 'footer.php';
+?>
