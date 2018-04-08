98a5711e3e7a69bf3415bc0ebdd1f07408d7e8ba
Kyle Berkley <berkyle@ku.edu>
2018-04-07 20:01:05 -0500

    Now creates new user in both Profiles and GameUsers database. Tried to do this with a function but DB connections apparently don't go through in PHP functions? It was a mess...

--- php/register.php
+++ php/register.php
@@ -1,65 +1,58 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Tactics - Create Account</title>
-  </head>
-  <body>
+<?php
+  require_once 'config.php';
+  include_once 'header.php';
 
-    <?php
-    require_once 'config.php';
+  $username = trim($_POST["username"]);
+  $password = $_POST['password'];
+  $confirm_password = $_POST['confirm_password'];
+  $username_err = $password_err = $confirm_password_err = "";
 
-    $username = $password = $confirm_password = "";
-    $username_err = $password_err = $confirm_password_err = "";
-
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        if(empty(trim($_POST["username"]))){
-            $username_err = "Please enter a valid username.";
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+      if(empty($username)){
+          $username_err = "Please enter a valid username.";
+      }
+      else {
+        if(strlen($password) < 8){
+          $password_err = "Password must have at least 8 characters.";
         }
         else {
-          $username = trim($_POST["username"]);
-          if(empty($_POST['password'])){
-            $password_err = "Please enter a password you like.";
-          }
-          elseif(strlen($_POST['password']) < 8){
-            $password_err = "Password must have atleast 8 characters.";
+          if($password != $confirm_password){
+              $confirm_password_err = 'Passwords must match.';
           }
           else {
-            $password = $_POST['password'];
-
-            $confirm_password = $_POST['confirm_password'];
-            if($password != $confirm_password){
-                $confirm_password_err = 'Passwords must match.';
+            if($link->connect_errno){
+              printf("Connect failed: %s\n", $link->connect_error);
+              exit();
             }
-            else {
-              if($link->connect_errno){
-                printf("Connect failed: %s\n", $link->connect_error);
-                exit();
-              }
-
-              $check = mysqli_query($link, "SELECT * FROM GameUsers WHERE username='$username'");
-        			$found = mysqli_num_rows($check);
-
-              if($found > 0) {
-                $username_err .= "That username already exists. Try again.";
-              }
+            $check = mysqli_query($link, "SELECT * FROM GameUsers WHERE username='$username'");
+      			$found = mysqli_num_rows($check);
+            if($found > 0) {
+              $username_err .= "That username already exists. Try again.";
             }
           }
         }
+      }
 
-        if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+      if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
 
-          $query = "INSERT IGNORE INTO GameUsers(username, password) VALUES('$username', '$password')";
-          if ($result = $link->query($query)) {
-            echo "<h3>New user created!</h3>";
-            echo "<h5><a href=\"../index.html\">Return to landing page...</a></h5>";
-            $result->free(); // free result set
-            $link->close(); /*close connection */
-          }
+        include_once 'userDB.php';
+        $queryGameUsers = "INSERT IGNORE INTO GameUsers(username, password) VALUES('$username', '$password')";
+        if ($result = $link->query($queryGameUsers)) {
+          echo "<h3>New user ";
         }
+        $queryProfiles = "INSERT IGNORE INTO Profiles(username, wins, draws, losses) VALUES('$username', '0', '0', '0')";
+        if ($result = $link->query($queryProfiles)) {
+          echo "created!</h3>";
+        }
+        echo "<h5><a href=\"../index.html\">Return to landing page...</a></h5>";
+        $result->free(); // free result set
+        $link->close(); /*close connection */
+      }
+      else{
         echo "<h3>".$username_err.$password_err.$confirm_password_err."</h3>";
-    }
-    ?>
-    <h5><a href="../index.html">Return to landing page...</a></h5>
-  </body>
-</html>
+      }
+  }
+  echo "<h5><a href=\"../index.html\">Return to landing page...</a></h5>";
+
+  include_once 'footer.php';
+?>
