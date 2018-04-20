fdc23a1ee00a3351fe5346f48214533abd6e2d0c
Kyle <berkyle@ku.edu>
2018-04-20 16:47:28 -0500

    php/header.php now will give you ninerStyle.css or simpleStyle.css no matter where you are in the directory as long as you use a file named simpleBoard.php or ninerBoard.php .

--- php/header.php
+++ php/header.php
@@ -16,18 +16,20 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href= <?php echo getDirectoryEscape()."css/index.css"; ?> >
+
     <?php
       $loadRules = false;
       $boardType = basename($_SERVER['REQUEST_URI'],'.php'); //gets name of URI file (file the URI begins building site with)
       if($boardType == "simpleBoard") {
-        echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"../css/simpleStyle.css\">";
+        echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"".getDirectoryEscape()."css/simpleStyle.css\">";
         $loadRules = true;
       }
       elseif($boardType == "ninerBoard") {
-        echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"../css/ninerStyle.css\">";
+        echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"".getDirectoryEscape()."css/ninerStyle.css\">";
         $loadRules = true;
       }
     ?>
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
