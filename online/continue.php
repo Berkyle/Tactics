f669f625ad0e21ca14ed81aa5522786070695d6f
Kyle <berkyle@ku.edu>
2018-04-22 19:52:30 -0500

    Now works for both 9x9 and 3x3.

--- online/continue.php
+++ online/continue.php
@@ -1,25 +1,51 @@
 <?php include_once '../php/header.php'; ?>
 
 <h1>Continue game:</h1>
-<div class="modal-body row">
-  <div class="col-md-6">
-    <?php include '../php/rankings.php'; ?>
+<h2>Your games:</h2>
+
+<div class="col-sm-4">
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading"><h2>Your 3x3 Matches</h2></div>
+      <div class="panel-body">
+        <?php
+        $query = mysqli_query($link, "SELECT * FROM ThreesBoards WHERE user1 = '$sessionUsr' OR user2 = '$sessionUsr'");
+        $numrows = 0 + mysqli_num_rows($query);
+
+        echo "<h3>you have ".$numrows." 3x3 games!</h3>";
+      
+        ?>
+      </div>
+    </div>
   </div>
-  <div class="col-md-6">
-    <h2>Your games:</h2>
+</div>
 
-    <?php
-    $query = mysqli_query($link, "SELECT * FROM ThreesBoards WHERE user1 = '$sessionUsr' OR user2 = '$sessionUsr'");
-    $profile = $link->query($query);
-    $numrows = mysqli_num_rows($query);
+<div class="col-sm-4">
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading"><h2>Rankings</h2></div>
+      <div class="panel-body">
+        <?php include '../php/rankings.php'; ?>
+      </div>
+    </div>
+  </div>
+</div>
 
-    echo "<h3>you have ".$numrows." games!</h3>";
-    // for(let $i = 0; $i < $numrows; i++) {
-    //   echo "<h3>you have ".$numrows." games!</h3>";
-    // }
+<div class="col-sm-4">
+  <div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading"><h2>Your 9x9 Matches</h2></div>
+      <div class="panel-body">
+        <?php
+        $query = mysqli_query($link, "SELECT * FROM NinesBoards WHERE user1 = '$sessionUsr' OR user2 = '$sessionUsr'");
+        $numrows = 0 + mysqli_num_rows($query);
 
-    ?>
+        echo "<h3>you have ".$numrows." 9x9 games!</h3>";
 
+        $link->close(); /*close connection */
+        ?>
+      </div>
+    </div>
   </div>
 </div>
 <?php include_once '../php/footer.php'; ?>
--- online/gameCreate.php
+++ online/gameCreate.php
@@ -2,7 +2,8 @@
   include_once '../php/header.php';
   $sessionUsr = $_COOKIE["user"];
   $inviteUsr  = $_POST["user2"];
-  $plyrMove   = $_POST["move"];
+  $plyrMove3   = $_POST["move3"];
+  $plyrMove9   = $_POST["move9"];
 
   if(strtolower($sessionUsr) == strtolower($inviteUsr)) {
     echo "<h1>You can't play with yourself here..</h1><h3>.... you should probably go outside..</h3>";
@@ -11,31 +12,40 @@
     $check = mysqli_query($link, "SELECT * FROM GameUsers WHERE username='$inviteUsr'");
     $found = mysqli_num_rows($check);
     if($found > 0) { //Check database for existing user with the same name
-      if($plyrMove >= 0 && $plyrMove <= 8) { //Check move validity
-        //At this point, all inputs valid.
-        //Make that game!
+      //At this point, all inputs valid.
+      //Make that game!
 
-        $nextID = mysqli_num_rows(mysqli_query($link, "SELECT * FROM ThreesBoards")) + 1;
-
-        //Create row in game board table
-        $queryTable = "INSERT IGNORE INTO ThreesBoards(user1, user2) VALUES('$sessionUsr', '$inviteUsr')";
-        if ($result = $link->query($queryTable)) {
-          echo "<h1>New game created!</h1>";
-        }
-
-        //Create row in game move table
-        $queryMove = "INSERT IGNORE INTO ThreesMoves(game_id, isX, moveNumber, movePosition) VALUES('$nextID', '1', '1', '$plyrMove')";
-        if ($result = $link->query($queryMove)) {
-          echo "<h4>User ".$inviteUsr." has received your challenge!</h4>";
-        }
-
-        echo "<a href=\"../\"><button type=\"button\" class=\"btn btn-default btn-lg\">Home</button></a>";
-        $result->free(); // free result set
-        $link->close(); /*close connection */
+      if($plyrMove3 == "") {
+        $boardTable = "NinesBoards";
+        $moveTable = "NinesMoves";
+        $plyrMove = $plyrMove9;
       }
       else {
-        echo "<h1>what the... hey bud that's no good...</h1>";
+        $boardTable = "ThreesBoards";
+        $moveTable = "ThreesMoves";
+        $plyrMove = $plyrMove3;
       }
+
+      if($boardTable == "ThreesBoards") $moveTable = "ThreesMoves";
+      else $moveTable = "NinesMoves";
+
+      $nextID = mysqli_num_rows(mysqli_query($link, "SELECT * FROM $boardTable")) + 1;
+
+      //Create row in game board table
+      $queryTable = "INSERT IGNORE INTO ".$boardTable."(user1, user2) VALUES('$sessionUsr', '$inviteUsr')";
+      if ($result = $link->query($queryTable)) {
+        echo "<h1>New game created!</h1>";
+      }
+
+      //Create row in game move table
+      $queryMove = "INSERT IGNORE INTO ".$moveTable."(gameID, isX, moveNumber, movePosition) VALUES('$nextID', '1', '1', '$plyrMove')";
+      if ($result = $link->query($queryMove)) {
+        echo "<h4>User ".$inviteUsr." has received your challenge!</h4>";
+      }
+
+      echo "<a href=\"../\"><button type=\"button\" class=\"btn btn-default btn-lg\">Home</button></a>";
+      $result->free(); // free result set
+      $link->close(); /*close connection */
     }
     else { //Invited user does not exist
       echo "<h1>User ".$inviteUsr." does not exist...</h1>
