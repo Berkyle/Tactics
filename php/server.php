9889fc8d87d79354112700117aee82356884d813
WeitingWei <35938073+WeitingWei@users.noreply.github.com>
2018-04-25 17:46:37 -0500

    Add files via upload

---
+++ php/server.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+
+$username = "";
+$email    = "";
+$errors = array(); 
+
+
+$db = mysqli_connect('localhost', 'root', '', 'registration');
+
+// REGISTER USER
+if (isset($_POST['reg_user'])) {
+ 
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+  
+  if (empty($username)) { array_push($errors, "Please enter your username"); }
+  if (empty($email)) { array_push($errors, "Please enter your email"); }
+  if (empty($password_1)) { array_push($errors, "Please enter your password"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "Your two passwords don't match");
+  }
+
+  
+  
+  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['username'] === $username) {
+      array_push($errors, "Someone has taken the username");
+    }
+
+    if ($user['email'] === $email) {
+      array_push($errors, "Someone has already used this email");
+    }
+  }
+
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	$password = md5($password_1);
+
+  	$query = "INSERT INTO users (username, email, password) 
+  			  VALUES('$username', '$email', '$password')";
+  	mysqli_query($db, $query);
+  	$_SESSION['username'] = $username;
+  	$_SESSION['success'] = "Logged in!";
+  	header('location: index.php');
+  }
+}
