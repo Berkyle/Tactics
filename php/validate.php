6dae9254195b1eb5c980a9068696ea34cb77e079
Kyle <berkyle@ku.edu>
2018-04-28 22:03:03 -0500

    security update.

--- php/validate.php
+++ php/validate.php
@@ -7,7 +7,7 @@
   */
 
   if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $username = $_POST["username"];
+    $username = strtolower($_POST["username"]);
     $password = $_POST["password"];
 
     require_once 'config.php';
