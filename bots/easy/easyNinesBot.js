f5ab1ec190e2df790c6b2a3ae44c41c4ea4d42c4
Surabhi Khachar <surabhikhachar@ku.edu>
2018-05-02 14:51:22 -0500

    changing doc to yuidoc

--- bots/easy/easyNinesBot.js
+++ bots/easy/easyNinesBot.js
@@ -46,7 +46,7 @@ class easyBot {
   *
   * @method randomMove
   * @param none
-  * @return {validMoves} A temporary array of all the available spaces
+  * @return {number} Returns an integer that corresponds to a move from the array of valid moves
   */
   randomMove(){
     let validMoves = [];
