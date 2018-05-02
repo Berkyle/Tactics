d81165e29c5cf90b16127ab48ef199973600fc7a
Kyle <berkyle@ku.edu>
2018-05-02 08:58:42 -0500

    9x9 program in progress.

--- online/continueNines.php
+++ online/continueNines.php
@@ -21,7 +21,7 @@
           <h2>Your 9x9 game with ".htmlspecialchars($opponent)."</h2>";
 ?>
 
-  <form action="ninesSubmit.php" method="post">
+  <form action="ninesSubmit.php" id="gameForm" method="post">
     <h3>Choose your next move below:</h3>
     <div class="tableContainer">
       <div id="outter" class="outter">
@@ -31,12 +31,6 @@
           while($move = $moves->fetch_assoc())
           {
             array_push($AllMoves, $move["movePosition"]);
-          }
-
-          echo "oh no";
-
-          foreach($AllMoves as $someMoves) {
-            echo "<h1>$someMoves</h1>";
           }
 
           $GAME = new ninerBoard($userX, $userO, $AllMoves, $gameID);
--- online/ninesJS.js
+++ online/ninesJS.js
@@ -8,28 +8,26 @@ function addEvents() {
 
   try { //Try block will only work if in continue game mode
     let daMoves = JSON.parse(document.getElementById("ignoreMe").value);
-
+    console.log(daMoves);
+    //Catch Game and Page objects up with current game
     daMoves.forEach(function(i) {
-      if(!page.XO[i].classList.contains("selected") && !page.XO[i].classList.contains("grayed")){
         board.move(i);
         page.updateBoard(false, i, board.selClass);
         page.grayOthers(i);
         if(board.checkBoardFull(i%9)){
           page.removeGrayedAll();
         }
-        if(board.checkGameWin()){
+        if(board.checkGameWin()) {
           page.updateBoard(true, i, board.selClass);
           page.finishGame(board.winner);
         }
-      }
     });
+    //objects 'board' and 'page' are now up to date
 
     var nextMove = ((document.getElementById("ignoreMe").value)%2 == 0) ? "X" : "O";
-
   }
   catch(error) { //Catch block runs if game is being created
     var nextMove = "X";
-
   }
 
   let available = [];
@@ -40,6 +38,8 @@ function addEvents() {
   }
 
   //Update 'add[]' and 'available[]'
+  //'add' assists JS with finding the correct radio button to check
+  //'available' is a list of available tile positions
   for(let i = 0; i < 81; i++) {
     if(!XO[i].classList.contains("grayed") && !XO[i].classList.contains("selected")) //tile not taken
       available.push(i);
@@ -54,6 +54,7 @@ function addEvents() {
     document.getElementsByTagName("body")[0].innerHTML += "<input type=\"radio\" name=\"move9\" value=\""+tile+"\" class=\"radio online\" required>";
   });
 
+  //Add events for selecting a game move option
   for(let i = 0; i < 81; i++) {//81-segment loop - adds click event to each tile
     if(!XO[i].classList.contains("grayed") && !XO[i].classList.contains("selected")) {
       XO[i].addEventListener("click", function() {
@@ -75,40 +76,46 @@ function addEvents() {
     }
   }
 
-  //Wait for user to attempt to submit move
-  try {
-    document.getElementById("submit").addEventListener("click", function() {
-      let myInputs = document.getElementsByClassName("online");
-      let someChecked = false;
+  // //Wait for user to attempt to submit move
+  // try {
+  //   document.getElementById("submit").addEventListener("click", function() {
+  //     let myInputs = document.getElementsByClassName("online");
+  //     let someChecked = false;
+  //
+  //     for (let i = 0; i < available.length; i++) {
+  //       if(inputs[i].checked == true) {
+  //         someChecked = true;
+  //         break;
+  //       }
+  //     }
+  //
+  //     //Build 9-character game state string that that records if each sub-table is won
+  //     if(someChecked) { //only runs if a move was made
+  //       let DBGS = "";
+  //       for(let i = 0; i < 9; i++)
+  //       {
+  //         if(page.tables[i].classList.contains("Awinner"))  DBGS = DBGS + 'A'; //Board is a catscratch
+  //         else if (page.tables[i].classList.contains("Owinner"))  DBGS = DBGS + 'O'; //Board won by O
+  //         else if (page.tables[i].classList.contains("Xwinner"))  DBGS = DBGS + 'X'; //Board won by X
+  //         else DBGS = DBGS + 'N'; ////board not yet won
+  //       }
+  //
+  //       document.getElementById("ignoreMe").value = DBGS; //send state all sneaky-like >:)
+  //       document.getElementById("submit").value = board.winner;
+  //     }
+  //   });
+  // } catch(e) {
+  //   //nothing needs to happen, it's just a game create mode
+  // }
 
-      for (let i = 0; i < available.length; i++) {
-        if(inputs[i].checked == true) {
-          someChecked = true;
-          break;
-        }
-      }
-
-      //Build 9-character game state string that that records if each sub-table is won
-      if(someChecked) { //only runs if a move was made
-        let DBGS = "";
-        for(let i = 0; i < 9; i++)
-        {
-          if(page.tables[i].classList.contains("Awinner"))  DBGS = DBGS + 'A'; //Board is a catscratch
-          else if (page.tables[i].classList.contains("Owinner"))  DBGS = DBGS + 'O'; //Board won by O
-          else if (page.tables[i].classList.contains("Xwinner"))  DBGS = DBGS + 'X'; //Board won by X
-          else DBGS = DBGS + 'N'; ////board not yet won
-        }
-
-        document.getElementById("ignoreMe").value = DBGS; //send state all sneaky-like >:)
-        document.getElementById("submit").value = board.winner;
-      }
-    });
-  } catch(e) {
-    //nothing needs to happen, it's just a game creat mode
+  let myForm = document.getElementById("gameForm");
+  if(myForm.addEventListener) {
+    myForm.addEventListener("submit", checkState, false);  //Modern browsers
+  }
+  else if(myForm.attachEvent) {
+    myForm.attachEvent('onsubmit', checkState);            //Old IE
   }
 }
-
-
 
 
 
@@ -125,44 +132,67 @@ function checkState() {
     }
   }
 
+  if(moveVal == "") return false;
 
   let board = new ninerBoard();
   let page = new ninePageState(XO, tables, board);
 
+  try { //Try block will only work if in continue game mode
+    let daMoves = JSON.parse(document.getElementById("ignoreMe").value);
+    //Catch Game and Page objects up with current game
+    daMoves.forEach(function(i) {
+      // if(!page.XO[i].classList.contains("selected") && !page.XO[i].classList.contains("grayed")){
+        board.move(i);
+        // page.updateBoard(false, i, board.selClass);
+        // page.grayOthers(i);
+        if(board.checkBoardFull(i%9)){
+          //page.removeGrayedAll();
+        }
+        if(board.checkGameWin()) {
+          //page.updateBoard(true, i, board.selClass);
+          //page.finishGame(board.winner);
+        }
+      // }
+    });
+    //objects 'board' and 'page' are now up to date
+
+    var nextMove = ((document.getElementById("ignoreMe").value)%2 == 0) ? "X" : "O";
+    bourd.move(moveVal);
+
+    if(board.checkGameWin()) {
+      //page.updateBoard(true, i, board.selClass);
+      //page.finishGame(board.winner);
+    }
+  }
+  catch(error) { //Catch block runs if game is being created
+    var nextMove = "X";
+  }
+  document.getElementById("submit").value = available.length;
+
   board.move(moveVal);
   page.updateBoard(false, moveVal, board.selClass);
-
-  console.log(board.subBoards);
 
   if(board.checkGameWin()){
 		page.updateBoard(true, moveVal, board.selClass);
 		page.finishGame(board.winner);
   }
-  console.log(board.subBoards);
 
   let DBGS = "";
   for(let i = 0; i < 9; i++)
   {
-    if(page.tables[i].classList.contains("Awinner")) {
+    if(page.tables[i].classList.contains("Awinner"))
       DBGS = DBGS + 'A';
-    }
-    else if (page.tables[i].classList.contains("Owinner")) {
+    else if (page.tables[i].classList.contains("Owinner"))
       DBGS = DBGS + 'O';
-    }
-    else if (page.tables[i].classList.contains("Xwinner")) {
+    else if (page.tables[i].classList.contains("Xwinner"))
       DBGS = DBGS + 'X';
-    }
-    else { //board not yet won
+    else  //board not yet won
       DBGS = DBGS + 'N';
-    }
   }
-  console.log(DBGS);
-  console.log(board.subBoards);
 
   document.getElementById("ignoreMe").value = DBGS; //send state all sneaky-like >:)
-  document.getElementById("submit").value = board.winner;
+  if(board.winner != "") {
+    document.getElementById("submit").value = board.winner;
+  }
 
-  console.log(DBGS);
-
-  return false;
 }
