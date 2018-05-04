444d6dacd52ff751233a4cd5fcd911f1775c9d7e
Diego Garcia <diego.alejandro.garcia09@gmail.com>
2018-05-03 21:15:16 -0500

    Improve the tests

--- tests/nineByNineTest/mediumNinesBot.js
+++ tests/nineByNineTest/mediumNinesBot.js
@@ -1,10 +1,24 @@
-class hardBot {
+/**
+* This is the class for the medium bot for the 9x9 game.
+*
+* @class mediumBot
+* @constructor Initializes all variables that will change later in the program.
+*/
+class mediumBot {
   constructor(XO, board, page){
     this.XO = XO;
     this.board = board;
     this.page = page;
+    this.randomSwitch = true;
     this.typeOfMove;
   }
+  /**
+  * runBot decides what the bot's next move will be depending on the difficulty chosen by the user.
+  *
+  * @method runBot
+  * @param none
+  * @return none
+  */
   runBot(){
     let botMove = this.nextMove();
     if(!this.XO[botMove].classList.contains("selected") && !this.XO[botMove].classList.contains("grayed")){
@@ -30,28 +44,54 @@ class hardBot {
     }
     return botMove;
   }
+  /**
+  * Based on the returns of the other bot methods, it will return what the next move is going to be.
+  *
+  * @method runBot
+  * @param none
+  * @return {winningMove} If the bot sees a move on the board that allows it to win the game, it will return the position of the winning move for the bot.
+  * @return {blockMove} If the bot doesn't see a winning move but can block its opponent from winning, it will return the position of this block move.
+  * @return {twoTiles} If the bot doesn't see a block move or a winning move, it will return a move that allows it to have two tiles next to one another.
+  * @return {randomMove} If none of the above conditions are satisified, then the bot will perform a random move wherever it sees an opening position.
+  */
   nextMove(){
-    if(this.winningMove() != -1){
-      console.log("winning");
-      this.typeOfMove = "Winning";
-      return (this.winningMove());
+    if(this.randomSwitch == false){
+      this.randomSwitch = !this.randomSwitch;
+      if(this.winningMove() != -1){
+        console.log("winning");
+        this.typeOfMove = "Winning";
+        return (this.winningMove());
+      }
+      else if(this.blockMove() != -1){
+        console.log("blocked");
+        this.typeOfMove = "Blocking";
+        return (this.blockMove());
+      }
+      else if(this.twoTiles() != -1){
+        console.log("twoTiles");
+        this.typeOfMove = "TwoTiles";
+        return (this.twoTiles());
+      }
+      else{
+        console.log("random");
+        this.typeOfMove = "Random";
+        return (this.randomMove());
+      }
     }
-    else if(this.blockMove() != -1){
-      console.log("blocked");
-      this.typeOfMove = "Blocking";
-      return (this.blockMove());
-    }
-    else if(this.twoTiles() != -1){
-      console.log("twoTiles");
-      this.typeOfMove = "TwoTiles";
-      return (this.twoTiles());
-    }
-    else{
+    else if(this.randomSwitch == true){
+      this.randomSwitch = !this.randomSwitch;
       console.log("random");
       this.typeOfMove = "Random";
       return (this.randomMove());
     }
   }
+  /**
+  * randomMove looks for a random move in the board when called
+  *
+  * @method randomMove
+  * @param none
+  * @return {number} Returns an integer that corresponds to a move from the array of valid moves for the random move.
+  */
   randomMove(){
     let validMoves = [];
     for(let i = 0; i < 81; i++){
@@ -62,6 +102,13 @@ class hardBot {
     let random = Math.floor((Math.random() * validMoves.length));
     return validMoves[random];
   }
+  /**
+  * winningMove looks for a winning move in the board when called
+  *
+  * @method winningMove
+  * @param none
+  * @return {number} Returns an integer that corresponds to a move from the array of valid moves if there is a winning move. If there is no winning move, it will return -1.
+  */
   winningMove(){
     let validMoves = [];
     let OTiles = [];
@@ -118,6 +165,13 @@ class hardBot {
     }
     return -1;
   }
+  /**
+  * blockMove looks for a block move that blocks the user from winning when it is called
+  *
+  * @method blockMove
+  * @param none
+  * @return {number} Returns an integer that corresponds to a move from the array of valid moves if there is a block move to be made. Otherwise, it returns -1.
+  */
   blockMove(){
     let validMoves = [];
     let XTiles = [];
@@ -174,6 +228,13 @@ class hardBot {
     }
     return -1;
   }
+  /**
+  * twoTiles searches the board for a move that can be placed where there are two open tiles next to one another
+  *
+  * @method twoTiles
+  * @param none
+  * @return {number} Returns an integer that corresponds to a move from the array of valid moves. When there are no possible two tile moves, it returns -1
+  */
   twoTiles(){
     let validMoves = [];
     let OTiles = [];
--- tests/threebyThreeTest/threeBotTest.js
+++ tests/threebyThreeTest/threeBotTest.js
@@ -2,28 +2,31 @@ function addEvents() {
   let XO = document.getElementsByClassName("board"); // array of DOM tiles with indicies [0..8]
   let board = new simpleBoard();
   let page = new pageState(XO);
-  let bot = new hardBot(XO, board, page);
+  let bot = new easyBot(XO, board, page);
   let move;
 	let counrter = 0;
   let XorO;
+  let type;
 	for(let i = 0; i < 9; i++) {//9-segment loop - adds click event to each tile
     XO[i].addEventListener("click", function() {
       if(!this.classList.contains("XSelect") && !this.classList.contains("OSelect")){
         move = bot.runBot();
         XorO = board.XTurn ? "X" : "O";
+        type = bot.typeOfMove;
         counrter++;
         board.XTurn = !board.XTurn;
         page.updatePage(board);
         let testString = "<td class=\"testBox\">";
-        testString += ""+counrter+": "+move+"("+XorO+")  </td>";
+        testString += ""+counrter+": "+move+"("+XorO+")("+type+")</td>";
         document.getElementsByClassName("moveContainer")[0].innerHTML += testString;
         for(let i = 0; i < 9; i++){
           if(board.winner != ""){
             break;
           }
           move = bot.runBot();
+          type = bot.typeOfMove;
           counrter++;
-          testString = "<td class=\"testBox\">"+counrter+": "+move+"("+XorO+")  ";
+          testString = "<td class=\"testBox\">"+counrter+": "+move+"("+XorO+")("+type+")";
           XorO = board.XTurn ? "X" : "O";
           board.XTurn = !board.XTurn;
           page.updatePage(board);
@@ -40,28 +43,31 @@ function addEvents1() {
   let XO = document.getElementsByClassName("board1"); // array of DOM tiles with indicies [0..8]
   let board = new simpleBoard();
   let page = new pageState(XO);
-  let bot = new hardBot(XO, board, page);
+  let bot = new mediumBot(XO, board, page);
   let move;
 	let counrter = 0;
   let XorO;
+  let type;
 	for(let i = 0; i < 9; i++) {//9-segment loop - adds click event to each tile
     XO[i].addEventListener("click", function() {
       if(!this.classList.contains("XSelect") && !this.classList.contains("OSelect")){
         move = bot.runBot();
+        type = bot.typeOfMove;
         XorO = board.XTurn ? "X" : "O";
         counrter++;
         board.XTurn = !board.XTurn;
         page.updatePage(board);
         let testString = "<td class=\"testBox\">";
-        testString += ""+counrter+": "+move+"("+XorO+")  </td>";
+        testString += ""+counrter+": "+move+"("+XorO+")("+type+")</td>";
         document.getElementsByClassName("moveContainer1")[0].innerHTML += testString;
         for(let i = 0; i < 9; i++){
           if(board.winner != ""){
             break;
           }
           move = bot.runBot();
+          type = bot.typeOfMove;
           counrter++;
-          testString = "<td class=\"testBox\">"+counrter+": "+move+"("+XorO+")  ";
+          testString = "<td class=\"testBox\">"+counrter+": "+move+"("+XorO+")("+type+")";
           XorO = board.XTurn ? "X" : "O";
           board.XTurn = !board.XTurn;
           page.updatePage(board);
@@ -82,67 +88,32 @@ function addEvents2() {
   let move;
 	let counrter = 0;
   let XorO;
+  let type;
 	for(let i = 0; i < 9; i++) {//9-segment loop - adds click event to each tile
     XO[i].addEventListener("click", function() {
       if(!this.classList.contains("XSelect") && !this.classList.contains("OSelect")){
         move = bot.runBot();
+        type = bot.typeOfMove;
         XorO = board.XTurn ? "X" : "O";
         counrter++;
         board.XTurn = !board.XTurn;
         page.updatePage(board);
         let testString = "<td class=\"testBox\">";
-        testString += ""+counrter+": "+move+"("+XorO+")  </td>";
+        testString += ""+counrter+": "+move+"("+XorO+")("+type+")</td>";
         document.getElementsByClassName("moveContainer2")[0].innerHTML += testString;
         for(let i = 0; i < 9; i++){
           if(board.winner != ""){
             break;
           }
           move = bot.runBot();
+          type = bot.typeOfMove;
           counrter++;
-          testString = "<td class=\"testBox\">"+counrter+": "+move+"("+XorO+")  ";
+          testString = "<td class=\"testBox\">"+counrter+": "+move+"("+XorO+")("+type+")";
           XorO = board.XTurn ? "X" : "O";
           board.XTurn = !board.XTurn;
           page.updatePage(board);
           testString += " </td>";
           document.getElementsByClassName("moveContainer2")[0].innerHTML += testString;
-        }
-      }
-      // document.getElementsByClassName("moveContainer")[0].innerHTML += "</tr></table>";
-    });
-  }
-}
-
-function addEvents3() {
-  let XO = document.getElementsByClassName("board3"); // array of DOM tiles with indicies [0..8]
-  let board = new simpleBoard();
-  let page = new pageState(XO);
-  let bot = new hardBot(XO, board, page);
-  let move;
-	let counrter = 0;
-  let XorO;
-	for(let i = 0; i < 9; i++) {//9-segment loop - adds click event to each tile
-    XO[i].addEventListener("click", function() {
-      if(!this.classList.contains("XSelect") && !this.classList.contains("OSelect")){
-        move = bot.runBot();
-        XorO = board.XTurn ? "X" : "O";
-        counrter++;
-        board.XTurn = !board.XTurn;
-        page.updatePage(board);
-        let testString = "<td class=\"testBox\">";
-        testString += ""+counrter+": "+move+"("+XorO+")  </td>";
-        document.getElementsByClassName("moveContainer3")[0].innerHTML += testString;
-        for(let i = 0; i < 9; i++){
-          if(board.winner != ""){
-            break;
-          }
-          move = bot.runBot();
-          counrter++;
-          testString = "<td class=\"testBox\">"+counrter+": "+move+"("+XorO+")  ";
-          XorO = board.XTurn ? "X" : "O";
-          board.XTurn = !board.XTurn;
-          page.updatePage(board);
-          testString += " </td>";
-          document.getElementsByClassName("moveContainer3")[0].innerHTML += testString;
         }
       }
       // document.getElementsByClassName("moveContainer")[0].innerHTML += "</tr></table>";
